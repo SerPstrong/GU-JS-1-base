c245b72bf4f01495cc076363eb2383996e806d09
serpstrong <ommuts@gmail.com>
2019-10-07 14:04:47 +0500

    Chess JS rotate numerals

--- lesson5/js/chess.js
+++ lesson5/js/chess.js
@@ -6,16 +6,7 @@
 // chessTable.className = "table";
 //
 // let letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", ""];
-
-// const chessTr = document.createElement("tr");
-// chessTr.className = "tr";
-// chessTable.appendChild(chessTr);
-
-// const chessTd = document.createElement("td");
-// chessTd.className = "td";
-// chessTr.appendChild(chessTd);
-
-
+//
 // for (let row = 0; row < 10; row++) {
 //     const chessTr = document.createElement("tr");
 //     chessTr.className = "tr";
@@ -24,12 +15,28 @@
 //         const chessTd = document.createElement("td");
 //         chessTd.className = "td";
 //         chessTr.appendChild(chessTd);
-//         if ((row === 0 || row === 9) && (col !== 0 && col !== 9)) {
-//             chessTd.textContent = col;
+//         if ((row === 9) && (col !== 9)) {
+//             chessTd.textContent = letters[col];
 //             continue;
 //         }
-//         if (col === 0 || col === 9) {
-//             chessTd.textContent = letters[row];
+//         if ((row === 0) && (col !== 0)) {
+//             chessTd.textContent = letters[col];
+//             chessTd.classList.add("rotate");
+//             continue;
+//         }
+//         if (col === 0) {
+//             if (row === 0) {
+//                 continue;
+//             }
+//             chessTd.textContent = row;
+//             continue;
+//         }
+//         if (col === 9) {
+//             if (row === 9) {
+//                 continue;
+//             }
+//             chessTd.textContent = row;
+//             chessTd.classList.add("rotate");
 //             continue;
 //         }
 //
@@ -45,6 +52,7 @@
 //
 // chess.appendChild(chessTable);
 
+
 "use strict";
 
 let chess = document.getElementById("chess");
@@ -52,49 +60,55 @@ let chess = document.getElementById("chess");
 let chessTable = document.createElement("table");
 chessTable.className = "table";
 
-let letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", ""];
+function chessboard() {
+    let letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", ""];
+    let numbers = ["", "8", "7", "6", "5", "4", "3", "2", "1", ""];
 
-for (let row = 0; row < 10; row++) {
-    const chessTr = document.createElement("tr");
-    chessTr.className = "tr";
+    for (let row = 0; row < 10; row++) {
+        const chessTr = document.createElement("tr");
+        chessTr.className = "tr";
 
-    for (let col = 0; col < 10; col++) {
-        const chessTd = document.createElement("td");
-        chessTd.className = "td";
-        chessTr.appendChild(chessTd);
-        if ((row === 9) && (col !== 9)) {
-            chessTd.textContent = letters[col];
-            continue;
-        }
-        if ((row === 0) && (col !== 0)) {
-            chessTd.textContent = letters[col];
-            chessTd.classList.add("rotate");
-            continue;
-        }
-        if (col === 0) {
-            if (row === 0) {
+        for (let col = 0; col < 10; col++) {
+            const chessTd = document.createElement("td");
+            chessTd.className = "td";
+            chessTr.appendChild(chessTd);
+            if ((row === 9) && (col !== 9)) {
+                chessTd.textContent = letters[col];
                 continue;
             }
-            chessTd.textContent = row;
-            continue;
-        }
-        if (col === 9) {
-            if (row === 9) {
+            if ((row === 0) && (col !== 0)) {
+                chessTd.textContent = letters[col];
+                chessTd.classList.add("rotate");
                 continue;
             }
-            chessTd.textContent = row;
-            chessTd.classList.add("rotate");
-            continue;
+            if (col === 0) {
+                if (row === 0) {
+                    continue;
+                }
+                chessTd.textContent = numbers[row];
+
+                continue;
+            }
+            if (col === 9) {
+                if (row === 9) {
+                    continue;
+                }
+                chessTd.textContent = numbers[row];
+                chessTd.classList.add("rotate");
+                continue;
+            }
+
+            if ((row + col) % 2 === 0) {
+                chessTd.classList.add("grey");
+            } else {
+                chessTd.classList.add("black");
+            }
         }
 
-        if ((row + col) % 2 === 0) {
-            chessTd.classList.add("grey");
-        } else {
-            chessTd.classList.add("black");
-        }
+        chessTable.appendChild(chessTr);
     }
 
-    chessTable.appendChild(chessTr);
+    chess.appendChild(chessTable);
 }
 
-chess.appendChild(chessTable);
+chessboard();
